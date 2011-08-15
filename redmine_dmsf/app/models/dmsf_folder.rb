6a5c287a6049769bb280a495aa20d4d353d312bb
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-08-15 05:39:53 +0000

    * possibility to copy file
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/branches/devel_1.1.0@215 5e329b0b-a2ee-ea63-e329-299493fc886d

---
+++ redmine_dmsf/app/models/dmsf_folder.rb
@@ -0,0 +1,175 @@
+# Redmine plugin for Document Management System "Features"
+#
+# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class DmsfFolder < ActiveRecord::Base
+  unloadable
+  
+  cattr_reader :invalid_characters
+  @@invalid_characters = /\A[^\/\\\?":<>]*\z/
+  
+  belongs_to :project
+  belongs_to :folder, :class_name => "DmsfFolder", :foreign_key => "dmsf_folder_id"
+  has_many :subfolders, :class_name => "DmsfFolder", :foreign_key => "dmsf_folder_id", :order => "title ASC"
+  has_many :files, :class_name => "DmsfFile", :foreign_key => "dmsf_folder_id",
+           :conditions => { :deleted => false }
+  belongs_to :user
+  
+  validates_presence_of :title
+  validates_uniqueness_of :title, :scope => [:dmsf_folder_id, :project_id]
+  
+  validates_format_of :title, :with => @@invalid_characters,
+    :message => l(:error_contains_invalid_character)
+  
+  validate :check_cycle
+  
+  acts_as_event :title => Proc.new {|o| o.title},
+                :description => Proc.new {|o| o.description },
+                :url => Proc.new {|o| {:controller => "dmsf", :action => "show", :folder_id => o}},
+                :datetime => Proc.new {|o| o.updated_at },
+                :author => Proc.new {|o| o.user }
+  
+  def check_cycle
+    folders = []
+    self.subfolders.each {|f| folders.push(f)}
+    folders.each do |folder|
+      if folder == self.folder
+        errors.add(:folder, l(:error_create_cycle_in_folder_dependency))
+        return false 
+      end
+      folder.subfolders.each {|f| folders.push(f)}
+    end
+    return true
+  end
+  
+  def self.project_root_folders(project)
+    find(:all, :conditions => 
+        ["dmsf_folder_id is NULL and project_id = :project_id", {:project_id => project.id}], :order => "title ASC")
+  end
+  
+  def delete
+    return false if !self.subfolders.empty? || !self.files.empty?
+    destroy
+  end
+  
+  def dmsf_path
+    folder = self
+    path = []
+    while !folder.nil?
+      path.unshift(folder)
+      folder = folder.folder
+    end 
+    path
+  end
+  
+  def dmsf_path_str
+    path = self.dmsf_path
+    string_path = path.map { |element| element.title }
+    string_path.join("/")
+  end
+  
+  def notify?
+    return true if self.notification
+    return true if folder && folder.notify?
+    return false
+  end
+  
+  def notify_deactivate
+    self.notification = false
+    self.save!
+  end
+  
+  def notify_activate
+    self.notification = true
+    self.save!
+  end
+  
+  def self.directory_tree(project, current_folder = nil)
+    tree = [[l(:link_documents), nil]]
+    DmsfFolder.project_root_folders(project).each do |folder|
+      unless folder == current_folder
+        tree.push(["...#{folder.title}", folder.id])
+        directory_subtree(tree, folder, 2, current_folder)
+      end
+    end
+    return tree
+  end
+
+  def deep_file_count
+    file_count = self.files.length
+    self.subfolders.each {|subfolder| file_count += subfolder.deep_file_count}
+    file_count
+  end
+
+  def deep_size
+    size = 0
+    self.files.each {|file| size += file.size}
+    self.subfolders.each {|subfolder| size += subfolder.deep_size}
+    size
+  end
+
+  # To fullfill searchable module expectations
+  def self.search(tokens, projects=nil, options={})
+    tokens = [] << tokens unless tokens.is_a?(Array)
+    projects = [] << projects unless projects.nil? || projects.is_a?(Array)
+    
+    find_options = {:include => [:project]}
+    find_options[:order] = "dmsf_folders.updated_at " + (options[:before] ? 'DESC' : 'ASC')
+    
+    limit_options = {}
+    limit_options[:limit] = options[:limit] if options[:limit]
+    if options[:offset]
+      limit_options[:conditions] = "(dmsf_folders.updated_at " + (options[:before] ? '<' : '>') + "'#{connection.quoted_date(options[:offset])}')"
+    end
+    
+    columns = options[:titles_only] ? ["dmsf_folders.title"] : ["dmsf_folders.title", "dmsf_folders.description"]
+            
+    token_clauses = columns.collect {|column| "(LOWER(#{column}) LIKE ?)"}
+    
+    sql = (['(' + token_clauses.join(' OR ') + ')'] * tokens.size).join(options[:all_words] ? ' AND ' : ' OR ')
+    find_options[:conditions] = [sql, * (tokens.collect {|w| "%#{w.downcase}%"} * token_clauses.size).sort]
+    
+    project_conditions = []
+    project_conditions << (Project.allowed_to_condition(User.current, :view_dmsf_files))
+    project_conditions << "project_id IN (#{projects.collect(&:id).join(',')})" unless projects.nil?
+    
+    results = []
+    results_count = 0
+    
+    with_scope(:find => {:conditions => [project_conditions.join(' AND ')]}) do
+      with_scope(:find => find_options) do
+        results_count = count(:all)
+        results = find(:all, limit_options)
+      end
+    end
+    
+    [results, results_count]
+  end
+
+  private
+  
+  def self.directory_subtree(tree, folder, level, current_folder)
+    folder.subfolders.each do |subfolder|
+      unless subfolder == current_folder
+        tree.push(["#{"..." * level}#{subfolder.title}", subfolder.id])
+        directory_subtree(tree, subfolder, level + 1, current_folder)
+      end
+    end
+  end
+  
+end
+
---
+++ redmine_dmsf/init.rb
@@ -0,0 +1,110 @@
+# Redmine plugin for Document Management System "Features"
+#
+# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'redmine'
+require 'dispatcher'
+  
+Dispatcher.to_prepare :redmine_dmsf do
+    unless ProjectsHelper.included_modules.include?(ProjectTabsExtended)
+        ProjectsHelper.send(:include, ProjectTabsExtended)
+    end
+end
+
+Redmine::Plugin.register :redmine_dmsf do
+  name "DMSF"
+  author "Vít Jonáš"
+  description "Document Management System Features"
+  version "1.1.0 devel"
+  url "http://code.google.com/p/redmine-dmsf/"
+  author_url "mailto:vit.jonas@gmail.com"
+  
+  requires_redmine :version_or_higher => '1.1.0'
+  
+  settings  :partial => 'settings/dmsf_settings',
+            :default => {
+              "dmsf_max_file_upload" => "0",
+              "dmsf_max_file_download" => "0",
+              "dmsf_storage_directory" => "#{RAILS_ROOT}/files/dmsf",
+              "dmsf_zip_encoding" => "utf-8",
+              "dmsf_index_database" => "#{RAILS_ROOT}/files/dmsf_index",
+              "dmsf_stemming_lang" => "english",
+              "dmsf_stemming_strategy" => "STEM_NONE"
+            }
+  
+  menu :project_menu, :dmsf, { :controller => "dmsf", :action => "show" }, :caption => :menu_dmsf, :after => :activity, :param => :id
+  #delete_menu_item :project_menu, :documents
+  
+  activity_provider :dmsf_files, :class_name => "DmsfFileRevision", :default => true
+  
+  project_module :dmsf do
+    permission :view_dmsf_folders, {:dmsf => [:show]}
+    permission :user_preferences, {:dmsf_state => [:user_pref_save]}
+    permission :view_dmsf_files, {:dmsf => [:entries_operation, :entries_email],
+      :dmsf_files => [:show]}
+    permission :folder_manipulation, {:dmsf => [:new, :create, :delete, :edit, :save, :edit_root, :save_root]}
+    permission :file_manipulation, {:dmsf_files => [:create_revision, :delete, :lock, :unlock],
+      :dmsf_upload => [:upload_files, :upload_file, :commit_files], :dmsf_files_copy => [:new, :create]}
+    permission :file_approval, {:dmsf_files => [:delete_revision, :notify_activate, :notify_deactivate], 
+      :dmsf => [:notify_activate, :notify_deactivate]}
+    permission :force_file_unlock, {}
+  end
+  
+  Redmine::WikiFormatting::Macros.register do
+    desc "Wiki link to DMSF file:\n\n" +
+             "!{{dmsf(file_id)}}\n\n" +
+         "_file_id_ can be found in link for file download."
+         
+    macro :dmsf do |obj, args|
+      return nil if args.length < 1 # require file id
+      entry_id = args[0].strip
+      entry = DmsfFile.find(entry_id)
+      unless entry.nil? || entry.deleted
+        title = args[1] ? args[1] : entry.title
+        revision = args[2] ? args[2] : ""
+        return link_to "#{title}", :controller => "dmsf_files", :action => "show", :id => entry, :download => revision
+      end
+      nil
+    end
+  end
+  
+  Redmine::WikiFormatting::Macros.register do
+    desc "Wiki link to DMSF folder:\n\n" +
+             "!{{dmsff(folder_id)}}\n\n" +
+         "_folder_id_ may be missing. _folder_id_ can be found in link for folder opening."
+         
+    macro :dmsff do |obj, args|
+      if args.length < 1
+        return link_to l(:link_documents), :controller => "dmsf", :action => "show", :id => @project
+      else
+        entry_id = args[0].strip
+        entry = DmsfFolder.find(entry_id)
+        unless entry.nil?
+          title = args[1] ? args[1] : entry.title
+          return link_to "#{title}", :controller => "dmsf", :action => "show", :id => @project, :folder_id => entry
+        end
+      end
+      nil
+    end
+  end
+  
+end
+
+Redmine::Search.map do |search|
+  search.register :dmsf_files
+  search.register :dmsf_folders
+end
