302f54a44e83dcf8d05d69555a024698bbd7d310
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-08-16 12:14:56 +0000

    * file copy complete
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/branches/devel_1.1.0@219 5e329b0b-a2ee-ea63-e329-299493fc886d

--- redmine_dmsf/app/controllers/dmsf_files_copy_controller.rb
+++ redmine_dmsf/app/controllers/dmsf_files_copy_controller.rb
@@ -26,16 +26,25 @@ class DmsfFilesCopyController < ApplicationController
 
   def new
     @target_project = DmsfFile.allowed_target_projects_on_copy.detect {|p| p.id.to_s == params[:target_project_id]} if params[:target_project_id]
-    @target_project ||= @project
-    flash.now[:warning] = l(:warning_no_project_to_copy_file_to) if DmsfFile.allowed_target_projects_on_copy.blank?
+    @target_project ||= @project if User.current.allowed_to?(:file_manipulation, @project)
+    if DmsfFile.allowed_target_projects_on_copy.blank?
+      flash.now[:warning] = l(:warning_no_project_to_copy_file_to)
+    else
+      @target_project ||= DmsfFile.allowed_target_projects_on_copy[0]
+    end
+    
+    @target_folder = DmsfFolder.find(params[:target_folder_id]) unless params[:target_folder_id].blank?
+    @target_folder ||= @file.folder if @target_project == @project
+    
     render :layout => !request.xhr?
   end
 
   def create
     if request.post?
       @target_project = DmsfFile.allowed_target_projects_on_copy.detect {|p| p.id.to_s == params[:target_project_id]} if params[:target_project_id]
-      if !@target_project
-        # Error
+      unless @target_project
+        render_403
+        return
       end
       @target_folder = DmsfFolder.find(params[:target_folder_id]) unless params[:target_folder_id].blank?
       if !@target_folder.nil? && @target_folder.project != @target_project
@@ -44,7 +53,6 @@ class DmsfFilesCopyController < ApplicationController
 
       name = @file.name
       
-      new_revision = DmsfFileRevision.new
       file = DmsfFile.find_file_by_name(@target_project, @target_folder, name)
       if file.nil?
         file = DmsfFile.new
@@ -54,19 +62,22 @@ class DmsfFilesCopyController < ApplicationController
         file.notification = !Setting.plugin_redmine_dmsf["dmsf_default_notifications"].blank?
       else
         if file.locked_for_user?
-          # Error here
+          flash[:error] = l(:error_file_is_locked)
+          redirect_to :action => "new", :id => @file, :target_project_id => @target_project, :target_folder_id => @target_folder
+          return
         end
       end
 
       last_revision = @file.last_revision
       
+      new_revision = DmsfFileRevision.new
       new_revision.folder = @target_folder
       new_revision.file = file
       new_revision.user = User.current
       new_revision.name = name
       new_revision.title = @file.title
       new_revision.description = @file.description
-      #new_revision.comment = 
+      new_revision.comment = l(:comment_copied_from, :source => "#{@project.identifier}:#{@file.dmsf_path_str}") 
       new_revision.source_revision = last_revision
       new_revision.major_version = last_revision.major_version
       new_revision.minor_version = last_revision.minor_version
@@ -78,11 +89,28 @@ class DmsfFilesCopyController < ApplicationController
       if file.save && new_revision.save
         file.reload
       else
-        # Error
+        flash[:error] = error_messages_for(file)
+        redirect_to :action => "new", :id => @file, :target_project_id => @target_project, :target_folder_id => @target_folder
+        return
       end
 
       flash[:notice] = l(:notice_file_copied)
       log_activity(file, "was copied (is copy)")
+      
+      if params[:move]
+        if User.current.allowed_to?(:file_manipulation, @project)
+          if @file.delete
+            flash[:notice] = l(:notice_file_moved)
+            log_activity(@file, "was deleted")
+            DmsfMailer.deliver_files_deleted(User.current, [@file])
+          else
+            flash[:error] = l(:error_file_is_locked)
+          end
+        else
+          flash[:error] = l(:error_user_has_not_right_delete_file)
+        end
+      end
+      
       redirect_to :controller => "dmsf_files", :action => "show", :id => file
     end
   end
@@ -96,12 +124,6 @@ class DmsfFilesCopyController < ApplicationController
   def find_file
     @file = DmsfFile.find(params[:id])
     @project = @file.project
-  end
-
-  def check_project(entry)
-    if !entry.nil? && entry.project != @project
-      raise DmsfAccessError, l(:error_entry_project_does_not_match_current_project) 
-    end
   end
   
 end
